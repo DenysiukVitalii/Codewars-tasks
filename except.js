51cb0e95da51df1abd7245a9675bcb788126cb30
vitalii_denysiuk <dendoc97@gmail.com>
2017-01-21 22:45:08 +0200

    task: except

--- except.js
+++ except.js
@@ -1,11 +1,21 @@
-Array.prototype.except = function (arr) {
+var array = [1,2,3,4,5,6];
+
+Array.prototype.except = function (keys) {
+  var arr = this.map((i) => i);
+  if(!isFinite(keys)) {
+      keys.sort((a,b) => a - b).forEach((k, i) => { arr.splice(k - i, 1);});
+  }
+  else arr.splice(keys, 1);
+  return arr;
+}
+
+console.log(array.except([1,3]));
+console.log(array);
+
+
+/*Array.prototype.except = function (arr) {
   var myArray = [];
   for (var key in this) myArray[key] = this[key];
   (isFinite(arr)) ? myArray.splice(arr, 1) : arr.sort((a,b) => a - b).map((el, i) => {if (i != 0) el -= 1; myArray.splice(el, 1);});
   return myArray;
-}
-
-var array = [1,2,3,4,5,6];
-
-console.log(array.except([6,3]));
-console.log(array);
+}*/
