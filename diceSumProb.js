d7a3dbe96b3ac8776dfb02028104c54b8d8e3b5f
vitalii_denysiuk <dendoc97@gmail.com>
2017-01-21 19:17:39 +0200

    task: diceSumProb(universal)

--- diceSumProb.js
+++ diceSumProb.js
@@ -1,17 +1,75 @@
+/* --- good --- */
 function rolldiceSumProb(sumOfDice, countDice){
-    var dice = [1,2,3,4,5,6];
-    var allEvents = Math.pow(dice.length, countDice);
+
+    function events(sumOfDice, countDice) {
+      if (countDice == 0) return sumOfDice == 0 ? 1 : 0;
+      if (sumOfDice <= 0) return 0;
+      var count = 0;
+      for (var i = 1; i <= 6; i++) count += events(sumOfDice - i, countDice - 1);
+      return count;
+    }
+
+    return events(sumOfDice, countDice) / Math.pow(6, countDice);
+}
+
+console.log(rolldiceSumProb(8,3));
+
+/* --- bad --- */
+function rolldiceSumProb(sumOfDice, countDice){
+    var allEvents = Math.pow(6, countDice);
     var sum = 0;
     var count = 0;
+    var dice = [1,2,3,4,5,6];
+
+    if (countDice == 2) {
       for (var j = 0; j < dice.length; j++) {
-        for (var i = 0; i < dice.length - 1; i++) {
-          sum = dice[j] + dice[i + 1];
-        //  console.log(dice[j] + " + " +  dice[i + 1] +  " = " + sum);
+      for (var i = 0; i < dice.length; i++) {
+        sum = dice[j] + dice[i];
+        if (sum == sumOfDice) count++;
+      }
+      }
+    }
+
+    if (countDice == 3) {
+        for (var k = 0; k < dice.length; k++) {
+        for (var j = 0; j < dice.length; j++) {
+        for (var i = 0; i < dice.length; i++) {
+          sum = dice[k] + dice[j] + dice[i];
           if (sum == sumOfDice) count++;
         }
+        }
+        }
+    }
+
+    if (countDice == 4) {
+      for (var m = 0; m < dice.length; m++) {
+      for (var k = 0; k < dice.length; k++) {
+      for (var j = 0; j < dice.length; j++) {
+      for (var i = 0; i < dice.length; i++) {
+        sum = dice[m] + dice[k] + dice[j] + dice[i];
+        if (sum == sumOfDice) count++;
       }
-      //console.log(count);
+      }
+      }
+      }
+    }
+
+    if (countDice == 5) {
+      for (var u = 0; u < dice.length; u++) {
+      for (var m = 0; m < dice.length; m++) {
+      for (var k = 0; k < dice.length; k++) {
+      for (var j = 0; j < dice.length; j++) {
+      for (var i = 0; i < dice.length; i++) {
+        sum = dice[u] + dice[m] + dice[k] + dice[j] + dice[i];
+        if (sum == sumOfDice) count++;
+      }
+      }
+      }
+      }
+      }
+    }
     return count / allEvents;
 }
 
-console.log(rolldiceSumProb(8, 2));
+
+console.log(rolldiceSumProb(8,3));
