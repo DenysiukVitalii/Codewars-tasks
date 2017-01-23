8d245963e645aeb1335443500c5d331432ba72ac
vitalii_denysiuk <dendoc97@gmail.com>
2017-01-23 12:00:18 +0200

    task: smile-face

---
+++ smile-face.js
@@ -0,0 +1,15 @@
+function countSmileys(arr) {
+  var count = 0;
+  function check(str) {
+    if (str.length == 2) {
+      if ((str[0] == ":" || str[0] == ";") && (str[1] == ")" || str[1] == "D")) count++; else  return 0;
+    } else
+    if (str.length == 3) {
+      if ((str[0] == ":" || str[0] == ";") && (str[1] == "-" || str[1] == "~") && (str[2] == ")" || str[2] == "D")) count++; else  return 0;
+    } else return 0;
+    return count;
+  }
+  return arr.filter( (item) => check(item) > 0 ).length;
+}
+
+console.log(countSmileys([':)', ';(', ';}', ':-D']));
