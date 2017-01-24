d5335ea2c46ab8b08336c3a55731c9fbb202b6cf
vitalii_denysiuk <dendoc97@gmail.com>
2017-01-24 14:51:36 +0200

    task: remove-zeros(universal)

--- remove-zeros.js
+++ remove-zeros.js
@@ -1,9 +1,31 @@
-function removeZeros(arr) {
-  var arrIndex = [];
-  for (var i = 0; i < arr.length; i++) if (arr[i] == 0 || arr[i] == "0") arrIndex.push(i);
-  for (var i = 0; i < arrIndex.length; i++) arr.splice(arrIndex[i] - i, 1);
-  for (var i = 0; i < arrIndex.length; i++) arr.push(0);
-  return arr;
-}
+// function removeZeros(arr) {
+//   var arrIndex = [];
+//   for (var i = 0; i < arr.length; i++) if (arr[i] == 0 || arr[i] == "0") arrIndex.push(i);
+//   for (var i = 0; i < arrIndex.length; i++) arr.splice(arrIndex[i] - i, 1);
+//   for (var i = 0; i < arrIndex.length; i++) arr.push(0);
+//   return arr;
+// }
 
-console.log(removeZeros([7, 2, 3, 0, 4, 6, 0, 0, 13, 0, 78, 0, 0, 19, 14]));
+
+function removeZeros(arr) {
+ function isZero(i) {
+  var count = 0, isafe = i;
+    for (i;i < arr.length; i++) if(arr[i] === 0 || arr[i] === "0") count++;
+    if (count == (arr.length - isafe)) return 1;
+  }
+ for (var m = 0; m < arr.length; m++) {
+   for (var i = 0; i < arr.length; i++) {
+     if (isZero(i) == 1) break;
+     if (arr[i] === 0) {
+     for (var k = i; k < arr.length; k++) arr[k] = arr[k + 1];
+       arr[arr.length - 1] = 0;
+     } else
+     if (arr[i] === "0") {
+     for (var k = i; k < arr.length; k++) arr[k] = arr[k + 1];
+       arr[arr.length - 1] = "0";
+     }
+   }
+ }
+ return arr;
+}
+console.log(removeZeros([1,2,52,7,"3",1,"0",0,"0",0]));
