2753869a43b56d965f1f1d1bdde2d94320940e5e
vitalii_denysiuk <dendoc97@gmail.com>
2017-01-25 20:14:38 +0200

    task: encryption

--- encryption.js
+++ encryption.js
@@ -1,5 +1,4 @@
 function encrypt(sym, count) {
- return sym.split('').map(item => String.fromCharCode(item.charCodeAt() + count)).join('');
+ return sym.split('').map(item => String.fromCharCode((item.charCodeAt(0) + count) % 256)).join('');
 }
-
 console.log(encryption("please encrypt me", 2));
