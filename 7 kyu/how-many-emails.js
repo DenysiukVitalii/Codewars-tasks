f7da17e67f90c68e8799c452dae399a9525856fb
vitalii_denysiuk <dendoc97@gmail.com>
2017-02-18 21:56:22 +0200

    task: how-many-emails(best practices)

--- 7 kyu/how-many-emails.js
+++ 7 kyu/how-many-emails.js
@@ -5,4 +5,8 @@ function getPercentage(sent, limit = 1000){
   if (sent >= limit) return "Daily limit is reached";
   return parseInt(sent/limit*100) + "%";
 }
+
+function getPercentage(sent, limit = 1000){
+  return sent === 0 ? 'No e-mails sent' : sent >= limit ? 'Daily limit is reached' : Math.floor(sent/limit*100) + '%';
+}
 console.log(getPercentage(101, 1000));
