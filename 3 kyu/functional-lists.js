ba824048b4fc8442002635bf93a66ef989958a47
vitalii_denysiuk <dendoc97@gmail.com>
2017-02-20 23:32:04 +0200

    task: functional-lists

---
+++ 3 kyu/functional-lists.js
@@ -0,0 +1,126 @@
+"use strict";
+
+function List() {}
+
+function EmptyList() {}
+
+EmptyList.prototype = new List();
+EmptyList.prototype.constructor = EmptyList;
+
+EmptyList.prototype.toString = function() {
+	return "()";
+};
+EmptyList.prototype.isEmpty = function() {
+	return true;
+};
+
+EmptyList.prototype.length = function() {
+	return 0;
+};
+
+EmptyList.prototype.push = function(x) {
+	let listNode = new ListNode(x, this);
+	return listNode;
+};
+
+EmptyList.prototype.remove = function(x) {};
+EmptyList.prototype.append = function(xs) {};
+
+
+function ListNode(value, next) {
+	this.value = value;
+	this.next = next;
+}
+
+
+ListNode.prototype = new List();
+ListNode.prototype.constructor = ListNode;
+ListNode.prototype.isEmpty = function() {
+	if (!this.length()) return true;
+	return false;
+};
+
+ListNode.prototype.toString = function() {
+	let list = this,
+		values = [];
+	while (list instanceof ListNode) {
+		values.push(list.value);
+		list = list.next;
+	}
+	return "(" + values.join(" ") + ")";
+};
+
+ListNode.prototype.head = function() {
+	return this.value;
+};
+
+ListNode.prototype.tail = function() {
+	return this.next;
+};
+ListNode.prototype.length = function() {
+	let length = 0;
+	let list = this;
+	while (list instanceof ListNode) {
+		length++;
+		list = list.next;
+	}
+	return length;
+};
+
+ListNode.prototype.push = function(x) {
+	let listNode = new ListNode(x, this);
+	return listNode;
+};
+
+ListNode.prototype.remove = function(x) {
+  if (x == this.head()) return this.next;
+
+	let length = this.length(),
+	    list = this,
+	    prevNode = list;
+
+	if(list.value === x) {
+		list = list.next;
+		length--;
+	}
+
+  list.r = true;
+
+	while (length > 1) {
+		list = list.next;
+		list.r = true;
+		list.value === x ? prevNode.next = list.next : prevNode = list;
+		length--;
+	}
+
+	return this;
+};
+
+
+ListNode.prototype.append = function(xs) {
+	let length = this.length(),
+	    list = this,
+	    copyThis, headCopy, prevNode;
+
+	copyThis = new ListNode(list.value, undefined);
+	headCopy = copyThis;
+	prevNode = copyThis;
+	length--;
+	while (length >= 1) {
+		length--;
+		list = list.next;
+		copyThis = new ListNode(list.value, undefined);
+		prevNode.next = copyThis;
+		prevNode = copyThis;
+	}
+	prevNode.next = xs;
+	return headCopy;
+};
+
+
+let mt = new EmptyList();
+let l1 = mt.push('c').push('b').push('a');
+let l2 = l1.append(l1);
+let l3 = l1.remove('b');
+let l4 = l2.remove('b');
+console.log(l4.toString());
