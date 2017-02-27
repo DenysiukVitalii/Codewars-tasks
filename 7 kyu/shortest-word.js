2073247a519eb0af5c4420a238a2fd45dff227f0
vitalii_denysiuk <dendoc97@gmail.com>
2017-02-27 21:47:39 +0200

    task: shortest-word(best practices - spread)

--- 7 kyu/shortest-word.js
+++ 7 kyu/shortest-word.js
@@ -1,4 +1,5 @@
 'use strict';
 let findShort = s => s.split(' ').map(i => i.length).sort((a,b) => a - b)[0];
+let findShort = s => Math.min(...s.split(" ").map (s => s.length));
 
 console.log(findShort("bitcoin take over tshe world maybe wssso knows perhaps"));
