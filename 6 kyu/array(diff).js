cb73a1d93979a666f15794cd3fa27066fedbb50d
vitalii_denysiuk <dendoc97@gmail.com>
2017-03-17 19:49:42 +0200

    task: array diff(best practices)

--- 6 kyu/array(diff).js
+++ 6 kyu/array(diff).js
@@ -5,5 +5,8 @@ a.forEach( (i) => { if (i != b[0]) arr.push(i)} );
 return arr;
 }
 
+const array_diff = (a, b) => a.filter((x) => b.indexOf(x) == -1);
+
+
 
 console.log(array_diff([1,2,2,2,3],[2]));
