f989bbb20954c0ad5bb4e4e7b9837ca286122967
vitalii_denysiuk <dendoc97@gmail.com>
2017-05-23 23:33:07 +0300

    task: spin words(best practices)

--- 6 kyu/spin-words.js
+++ 6 kyu/spin-words.js
@@ -5,4 +5,11 @@ function spinWords(str){
   }).join(' ');
 }
 
-console.log(spinWords( "Hey fellow warriors" )); // returns "Hey wollef sroirraw" 
+//best practices
+
+const spinWords = (string) => string.replace(/\w{5,}/g, function(w) { return w.split('').reverse().join('') });
+
+const spinWords = (str) => str.split(' ').map( w => w.length<5 ? w : w.split('').reverse().join('') ).join(' ');
+
+
+console.log(spinWords( "Hey fellow warriors" )); // returns "Hey wollef sroirraw"
