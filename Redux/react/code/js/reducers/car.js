8d9ba0fcb07487850a355a4a9231a7f6d603e9ef
DenysiukVitalii <dendoc97@gmail.com>
2017-11-23 22:13:21 +0200

    learn and try redux

---
+++ Redux/react/code/js/reducers/car.js
@@ -0,0 +1,28 @@
+export default function () {
+    return [
+        {
+            id: 1,
+            model: "Audi",
+            speed: 234.45,
+            weight: 1.4,
+            desc: "R8 это замечательный спортивный автомобиль!",
+            img: "https://audi-kiev.com.ua/content/uploads/A8-new/a8-menu.jpg"
+        },
+        {
+            id: 2,
+            model: "BMW",
+            speed: 240,
+            weight: 1.5,
+            desc: "BMW это крутой спортивный автомобиль!",
+            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzupkJkSG9YEB-SkJfqzgM0pofRQ7cfLjdktC5uJPLG4P_GVb5mA"
+        },
+        {
+            id: 3,
+            model: "Mercedes",
+            speed: 286.1,
+            weight: 1.35,
+            desc: "Mercedes это комфортный автомобиль!",
+            img:"https://www.mercedes-benz.com/wp-content/uploads/sites/3/2017/10/mbvideocar-mercedes-amg-c-63s-estate-1280x720-848x477.jpg"
+        },
+    ];
+}
